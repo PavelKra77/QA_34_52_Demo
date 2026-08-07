94662d4858f93c79ade6e25e5bccb4bfbe6c632a
PavelKra77 <pav.kravets86@gmail.com>
2026-08-07 19:44:43 +0300

    PracticeFormTest

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+    static WebDriver driver;
+
+    public void setDriver(WebDriver wd){
+        driver = wd;
+
+    }
+
+    public void pause(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/tests/PracticeFormTests.java
@@ -0,0 +1,37 @@
+package tests;
+
+import dto.Student;
+import manager.AppManager;
+import org.testng.annotations.Test;
+import pages.FormsPage;
+import pages.HomePage;
+import pages.PracticeFormPage;
+
+public class PracticeFormTests extends AppManager {
+    @Test
+    public void practiceFormPositiveTest(){
+        Student student = Student.builder()
+                .firstName("Polina")
+                .lastName("Polina")
+                .email("polina345@gmail.com")
+                .gender("female")
+                .mobile("0123456789")
+                .dateOfBirth("22 Apr 2003")
+                .subjects("Maths,Chemistry,English")
+                .hobbies("sport")
+                .picture("")
+                .address("Street 1")
+                .state("NCR")
+                .city("City")
+                .build();
+       // HomePage homePage = new HomePage(getDriver());
+       // homePage.clickBtnForms();
+        new HomePage(getDriver()).clickBtnForms();
+        new FormsPage(getDriver()).clickBtnPracticeForms();
+        PracticeFormPage practiceFormPage = new PracticeFormPage(getDriver());
+        practiceFormPage.typePracticeForm(student);
+
+
+    }
+
+}
